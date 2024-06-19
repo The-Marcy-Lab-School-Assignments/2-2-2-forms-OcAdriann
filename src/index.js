5a895778017d1fc7e58bc155758c709a12abe935
OcAdriann <ocadriannnc20@gmail.com>
2024-06-19 14:22:51 -0400

    All done

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ const handleSubmit = (e) => {
     const form = e.target
     const formValues = Object.fromEntries(new FormData(form))
     console.log(formValues)
+    const username = document.querySelector('#results-username')
+    username.textContent = formValues.username;
+    const codingLevel = document.querySelector('#results-coding-level')
+    codingLevel.textContent = formValues.codingLevel
+    const location = document.querySelector('#results-location')
+    location.textContent = formValues.location;
+    const assignment = document.querySelector('#results-did-like-assignment')
+    formValues.didLikeAssignment = Boolean(formValues.didLikeAssignment)
+    if(formValues.didLikeAssignment) {
+        assignment.textContent = 'Yes'
+    } else {
+        assignment.textContent = 'No'
+    }
     form.reset();
 };
 
