71d0171bdeb5106d376f97915e47899b684c9b59
OcAdriann <ocadriannnc20@gmail.com>
2024-06-19 12:50:06 -0400

    progress so far

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
  *
  */
 
-const handleSubmit = () => {
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target
+    const formValues = Object.fromEntries(new FormData(form))
+    console.log(formValues)
+    form.reset();
 };
+
+document.querySelector('form').addEventListener('submit', handleSubmit);
